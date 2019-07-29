5b744c52e377547e1799cd53595a6dcd2b73e52a
Florian <gagnon.florian@gmail.com>
2019-07-29 14:01:43 +0200

    improved views with forms

--- elo/views.py
+++ elo/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from elo.models import Player
 from elo.serializers import PlayerSerializer
 
 # Create your views here.
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def player_list(request):
     """
     List all code players, or create a new player.
@@ -14,17 +13,16 @@ def player_list(request):
     if request.method == 'GET':
         players = Player.objects.all()
         serializer = PlayerSerializer(players, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PlayerSerializer(data=data)
+        serializer = PlayerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def player_detail(request, pk):
     """
     Retrieve, update or delete a code player.
@@ -32,20 +30,19 @@ def player_detail(request, pk):
     try:
         player = Player.objects.get(pk=pk)
     except Player.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PlayerSerializer(player)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PlayerSerializer(player, data=data)
+        serializer = PlayerSerializer(player, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         player.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
