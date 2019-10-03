5fb9941c85500e813be051a06ae66e39df2c3b33
Christophe <christophe.github@gmail.com>
2019-10-03 12:59:19 +0200

    Created first component, the match form

---
+++ elo-ui/src/app/create-match/create-match.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-match',
+  templateUrl: './create-match.component.html',
+  styleUrls: ['./create-match.component.less']
+})
+export class CreateMatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
