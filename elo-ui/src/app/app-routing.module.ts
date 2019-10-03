a6c97f3ae0cbbf154270b797a67feb55968df02e
Christophe <christophe.github@gmail.com>
2019-10-03 13:17:44 +0200

    Added routes

--- elo-ui/src/app/app-routing.module.ts
+++ elo-ui/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CreateMatchComponent } from './create-match/create-match.component';
+import { ViewPlayerComponent } from './view-player/view-player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-match', component: CreateMatchComponent },
+  { path: 'player/:id',      component: ViewPlayerComponent },
+  { path: '',
+    redirectTo: '/create-match',
+    pathMatch: 'full'
+  },
+  { path: '**', component: CreateMatchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
